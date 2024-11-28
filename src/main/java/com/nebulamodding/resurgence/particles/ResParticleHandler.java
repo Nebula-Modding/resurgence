e8017fa7d53c0b81052616e8ba6cbb2ca858ca77
theognoodle <zclarinet4@gmail.com>
2024-11-28 12:20:31 -0800

    She Lode on my Stone till I Particle

--- src/main/java/com/nebulamodding/resurgence/particles/ResParticleHandler.java
+++ src/main/java/com/nebulamodding/resurgence/particles/ResParticleHandler.java
@@ -1,7 +1,37 @@
 package com.nebulamodding.resurgence.particles;
 
+import java.awt.*;
+import java.util.*;
+import java.math.*;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import team.lodestar.lodestone.registry.common.particle.LodestoneParticleTypes;
+import team.lodestar.lodestone.systems.easing.Easing;
+import team.lodestar.lodestone.systems.particle.builder.WorldParticleBuilder;
+import team.lodestar.lodestone.systems.particle.data.GenericParticleData;
+import team.lodestar.lodestone.systems.particle.data.color.ColorParticleData;
+import team.lodestar.lodestone.systems.particle.data.spin.SpinParticleData;
+import team.lodestar.lodestone.systems.particle.world.LodestoneWorldParticle;
+import team.lodestar.lodestone.systems.particle.world.type.LodestoneWorldParticleType;
+
+
+
 public class ResParticleHandler {
 
+    public static void spawnExampleParticles(Level level, Vec3 pos) {
+        Color startingColor = new Color(100, 0, 100);
+        Color endingColor = new Color(0, 100, 200);
+        WorldParticleBuilder.create(LodestoneParticleTypes.WISP_PARTICLE)
+                .setScaleData(GenericParticleData.create(0.5f, 0).build())
+                .setTransparencyData(GenericParticleData.create(0.75f, 0.25f).build())
+                .setColorData(ColorParticleData.create(startingColor, endingColor).setCoefficient(1.4f).setEasing(Easing.BOUNCE_IN_OUT).build())
+                .setSpinData(SpinParticleData.create(0.2f, 0.4f).setSpinOffset((level.getGameTime() * 0.2f) % 6.28f).setEasing(Easing.QUARTIC_IN).build())
+                .setLifetime(40)
+                .addMotion(0, 0.01f, 0)
+                .enableNoClip()
+                .spawn(level, pos.x, pos.y, pos.z);
+    }
 
 
 
