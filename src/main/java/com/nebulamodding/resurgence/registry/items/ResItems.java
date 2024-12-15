ddcbac678a8ea73e08f417333eda33d0840a0296
theognoodle <zclarinet4@gmail.com>
2024-12-14 17:03:55 -0800

    You Have Been Here Many Times

--- src/main/java/com/nebulamodding/resurgence/registry/items/ResItems.java
+++ src/main/java/com/nebulamodding/resurgence/registry/items/ResItems.java
@@ -11,7 +11,8 @@ public class ResItems {
 
     public static final DeferredItem<Item> VOLERMITE = ITEMS.register("volermite",
             ()-> new Item(new Item.Properties()));
-
+    public static final DeferredItem<Item> EYE_OF_ABYSS = ITEMS.register("eye_of_abyss",
+            ()-> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus)
     {
